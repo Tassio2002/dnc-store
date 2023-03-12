ebe0151143d37412ba459efbe48f822c0af6b72f
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-03-12 01:27:44 -0300

    feat: login static page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 
-import { HeaderLogin } from './styles';
+import {
+  HeaderLogin,
+  MainContainer,
+  LoginTitle,
+  LoginSubtitle,
+  LoginForm,
+  LoginLabel,
+  UserInput,
+  PasswordInput,
+  LoginButton,
+} from './styles';
 import logo from '../../assets/images/logo.png';
 
 function LoginPage() {
   return (
-    <HeaderLogin>
-      <img src={logo} alt="" />
-    </HeaderLogin>
+    <>
+      <HeaderLogin>
+        <img src={logo} alt="" />
+      </HeaderLogin>
+      <MainContainer>
+        <LoginTitle>Acesse com seu login ou cadastre-se!</LoginTitle>
+        <LoginSubtitle>você pode entrar com o seu CPF</LoginSubtitle>
+        <LoginForm>
+          <LoginLabel>Digite seu CPF:</LoginLabel>
+          <UserInput />
+          <LoginLabel>Senha:</LoginLabel>
+          <PasswordInput />
+          <LoginButton>Entrar</LoginButton>
+        </LoginForm>
+      </MainContainer>
+    </>
   );
 }
 
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -2,6 +2,83 @@ import styled from 'styled-components';
 
 export const HeaderLogin = styled.header`
   width: 100vw;
-  padding: 2em;
+  padding: 2em 3em;
   background-color: #2d2d2d;
+`;
+
+export const MainContainer = styled.main`
+  width: 100%;
+  padding-top: 3em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const LoginTitle = styled.h2`
+  padding-bottom: 0.3em;
+  font-size: 30px;
+  color: #000;
+`;
+
+export const LoginSubtitle = styled.p`
+  font-size: 22px;
+  margin-bottom: 4em;
+  color: #000;
+`;
+
+export const LoginForm = styled.form`
+  width: 100%;
+  max-width: 542px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const LoginLabel = styled.label`
+  padding-bottom: 0.5em;
+  font-size: 20px;
+  font-weight: 400;
+  color: #000;
+`;
+
+export const UserInput = styled.input.attrs({
+  type: 'text',
+  placeholder: 'Digite seu CPF',
+})`
+  padding: 1em;
+  border: 2px solid #7a7a7a;
+  border-radius: 10px;
+  margin-bottom: 2em;
+  font-size: 14px;
+  color: #616161;
+  background: rgba(255, 255, 255, 0.9);
+  ::placeholder,
+  ::-webkit-input-placeholder {
+    color: #616161;
+  }
+`;
+
+export const PasswordInput = styled.input.attrs({
+  type: 'password',
+  placeholder: '***************',
+})`
+  padding: 1em;
+  border: 2px solid #7a7a7a;
+  border-radius: 10px;
+  margin-bottom: 3.5em;
+  font-size: 14px;
+  color: #616161;
+  background: rgba(255, 255, 255, 0.9);
+  ::placeholder,
+  ::-webkit-input-placeholder {
+    color: #616161;
+  }
+`;
+
+export const LoginButton = styled.button`
+  padding: 1em 0;
+  border: none;
+  border-radius: 6px;
+  font-weight: 800;
+  background-color: #0c70f2;
+  color: #fff;
 `;
