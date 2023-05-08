6fd70bb5298b34c836e9ac745141f946d9a98f15
Tassio <81255475+Tassio2002@users.noreply.github.com>
2023-05-08 16:00:12 -0300

    feat/products data

---
+++ src/data/products.js
@@ -0,0 +1,57 @@
+import image1 from '../assets/images/image 21.png';
+import image2 from '../assets/images/image 22.png';
+import image3 from '../assets/images/image 23.png';
+import image4 from '../assets/images/image 24.png';
+import image5 from '../assets/images/image 25.png';
+import image6 from '../assets/images/image 26.png';
+
+export const products = [
+  {
+    id: 1,
+    name: 'Microsoft Xbox Series X | Preto',
+    description: 'Lorem ipsum, dolor sit amet consectetur',
+    price: 2000,
+    color: 'Preto',
+    image: image1,
+  },
+  {
+    id: 2,
+    name: 'Microsoft Xbox Series S | Branco',
+    description: 'Lorem ipsum, dolor sit amet consectetur',
+    price: 2000,
+    color: 'Branco',
+    image: image2,
+  },
+  {
+    id: 3,
+    name: 'Nintendo Switch com Jogo',
+    description: 'Lorem ipsum, dolor sit amet consectetur',
+    price: 2000,
+    color: 'Branco',
+    image: image3,
+  },
+  {
+    id: 4,
+    name: 'Jogo God of War Playstation',
+    description: 'Lorem ipsum, dolor sit amet consectetur',
+    price: 200,
+    color: 'Branco',
+    image: image4,
+  },
+  {
+    id: 5,
+    name: 'Jogo Uncharted: The Nathan Drake Collection',
+    description: 'Lorem ipsum, dolor sit amet consectetur',
+    price: 800,
+    color: 'Branco',
+    image: image5,
+  },
+  {
+    id: 6,
+    name: 'Jogo Gran Turismo',
+    description: 'Lorem ipsum, dolor sit amet consectetur',
+    price: 189,
+    color: 'Branco',
+    image: image6,
+  },
+];
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,14 +5,10 @@ import NavMenu from '../../components/global/NavMenu';
 import HeaderImage from '../../components/global/Header';
 import ProductCard from '../../components/ProductCard';
 import image1 from '../../assets/images/image 21.png';
-import image2 from '../../assets/images/image 22.png';
-import image3 from '../../assets/images/image 23.png';
-import image4 from '../../assets/images/image 24.png';
-import image5 from '../../assets/images/image 25.png';
-import image6 from '../../assets/images/image 26.png';
 import Footer from '../../components/global/Footer';
 import Modal from '../../components/global/Modal';
 import ProductInCart from '../../components/ProductInCart';
+import { products } from '../../data/products';
 
 function HomePage() {
   return (
@@ -31,21 +27,9 @@ function HomePage() {
       <NavMenu />
       <HeaderImage />
       <ProductsSection>
-        <ProductCard
-          image={image1}
-          description="Microsoft Xbox Series X | Preto"
-        />
-        <ProductCard
-          image={image2}
-          description="Microsoft Xbox Series S | Branco"
-        />
-        <ProductCard image={image3} description="Nintendo Switch com Jogo" />
-        <ProductCard image={image4} description="Jogo God of War Playstation" />
-        <ProductCard
-          image={image5}
-          description="Jogo Uncharted: The Nathan Drake Collection"
-        />
-        <ProductCard image={image6} description="Jogo Gran Turismo" />
+        {products.map((product) => (
+          <ProductCard image={product.image} description={product.name} />
+        ))}
       </ProductsSection>
       <Footer />
     </Body>
